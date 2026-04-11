2a4427bf9ca8fd9d0c823a36b6779ace2a7bd83c
Sabarivasan <sabarivasan1239@gmail.com>
2026-04-11 11:47:20 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
